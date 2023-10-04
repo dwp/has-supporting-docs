4ec1663d9c555e614550df83b6cd6115e9b43710
ben <ben.raine@engineering.digital.dwp.gov.uk>
2023-10-04 09:34:12 +0100

    first push

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
+
+window.GOVUKPrototypeKit.documentReady(() => {
+  // Add JavaScript here
+})
+
+import { initAll } from '@ministryofjustice/frontend'
+
+window.$ = $
+initAll()
